3798e17d614e973c552666e105ff4bf2508b8917
ismailofuz <sirojiddin.ismoilov7181@gmail.com>
2022-09-10 13:31:47 +0500

    fix: e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('ProductController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/product (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/product')
       .expect(200)
-      .expect('Hello World!');
+      .expect('This action returns all product');
   });
 });
